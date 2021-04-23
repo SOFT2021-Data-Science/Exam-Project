2eec25af750e94744866864a97f166fd11d496ab
Mutestock <doublenegative@protonmail.com>
2021-04-23 15:14:42 +0200

    pre-merge

--- back/diagram_webservice/src/main.py
+++ back/diagram_webservice/src/main.py
@@ -2,16 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "stuff works"
 
-@app.route('/stuff')
+
+@app.route("/stuff")
 def stuff_route():
     return "some other route works"
 
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=True)
-    
-    
--- back/diagram_webservice/tests/test_diagram_webservice.py
+++ back/diagram_webservice/tests/test_diagram_webservice.py
@@ -1,7 +1,7 @@
 from src import __version__
 import unittest
 
+
 class TestStrings(unittest.TestCase):
     def test_upper(self):
         self.assertEqual("spam".upper(), "SPAM")
-    
