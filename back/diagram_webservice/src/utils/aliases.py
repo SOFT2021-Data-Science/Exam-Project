c44e177ff41204dae121313c365f97700b3378e6
Mutestock <doublenegative@protonmail.com>
2021-05-08 15:42:21 +0200

    auto generation of out dir

--- back/diagram_webservice/src/utils/aliases.py
+++ back/diagram_webservice/src/utils/aliases.py
@@ -9,5 +9,8 @@ RESOURCES_DIR = os.path.dirname(RESOURCES_DIR) + "/resources"
 _DATASET_DIR = RESOURCES_DIR + "/datasets"
 OUT_DIR = RESOURCES_DIR + "/out"
 
+if not os.path.isdir(OUT_DIR):
+    os.mkdir(OUT_DIR)
+
 
 DATASETS = {"sdg": f"{_DATASET_DIR}/SDGSUICIDE,SDG_SH_STA_SCIDEN.csv"}
