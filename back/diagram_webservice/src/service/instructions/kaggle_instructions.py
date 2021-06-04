dc258cf77cadaa8bc03a1293250851c38a20d5b7
Mutestock <doublenegative@protonmail.com>
2021-06-04 12:42:03 +0200

    models implemented

--- back/diagram_webservice/src/service/instructions/kaggle_instructions.py
+++ back/diagram_webservice/src/service/instructions/kaggle_instructions.py
@@ -25,26 +25,56 @@ class KaggleInstruction(AbstractInstruction):
         kmeans_clustering_params = {
             "country": Param(country, "enum").as_json(),
             "gender": Param(genders, "enum").as_json(),
-            "clusters": Param([1,2,3,4,5,6,7,8,9,10], "range").as_json(),
+            "clusters": Param([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "range").as_json(),
         }
         kmeans_elbow_params = {
             "country": Param(country, "enum").as_json(),
             "gender": Param(genders, "enum").as_json(),
-            "clusters": Param([1,2,3,4,5,6,7,8,9,10], "range").as_json(),
+            "clusters": Param([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "range").as_json(),
+        }
+        comparison_genders_single_country_params = {
+            "country": Param(country, "enum").as_json(),
+        }
+        comparison_two_countries = {
+            "country_1": Param(country, "enum").as_json(),
+            "country_2": Param(country, "enum").as_json(),
+            "gender": Param(genders, "enum").as_json(),
         }
 
         linear_regression_model = Model(
-            "linear_regression", linear_regression_params,
+            "linear_regression",
+            linear_regression_params,
         ).as_json()
+
         kmeans_clustering_model = Model(
             "kmeans/clustering", kmeans_clustering_params
         ).as_json()
+
         kmeans_elbow_model = Model("kmeans/elbow", kmeans_elbow_params).as_json()
+
+        compare_country_comparison_genders_model = Model(
+            "compare/country_comparison_genders",
+            comparison_genders_single_country_params,
+        ).as_json()
+
+        compare_countries_comparison = Model(
+            "compare/countries_comparison", comparison_two_countries
+        ).as_json()
+
         self.dataset_name = "kaggle"
-        self.models = [linear_regression_model, kmeans_clustering_model, kmeans_elbow_model]
+
+        self.models = [
+            linear_regression_model,
+            kmeans_clustering_model,
+            kmeans_elbow_model,
+            compare_country_comparison_genders_model,
+            compare_countries_comparison
+        ]
+
         self.dataset_link = (
             "https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016"
         )
+
         self.description = """
         Content:
         
