6059044a5c4900ad0aa7a8690a857bfb3cf72d24
Jonas Hein <jonas.hein25@gmail.com>
2021-06-04 15:55:38 +0200

    Update About.vue

--- front/diagram_display/src/views/About.vue
+++ front/diagram_display/src/views/About.vue
@@ -122,7 +122,7 @@
           <b>K-Means clustering</b>
           “K-Means clustering intends to partition n objects into k clusters in which each object belongs to the cluster with the nearest mean. This method produces exactly k different clusters of greatest possible distinction. The best number of clusters k leading to the greatest separation (distance) is not known as a priori and must be computed from the data. The objective of K-Means clustering is to minimize total intra-cluster variance, or, the squared error function:”<br />
           
-          <img src="@/assets/k1.png" alt="">
+          <img src="@/assets/k1.png" alt=""><br /> 
           
           https://www.saedsayad.com/clustering_kmeans.htm
           <br /> 
